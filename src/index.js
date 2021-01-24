32d09acc25941b74508bea2c661420c6484346f9
JAGH <jagh1729@gmail.com>
2021-01-24 17:01:01 +0100

    Basic form with JSON stringify and validation

--- src/index.js
+++ src/index.js
@@ -3,11 +3,53 @@ import ReactDOM from 'react-dom';
 
 
 class MiniFormik extends React.Component {
-  state = {};
+  state = {
+    values: this.props.initialValues || {},
+    touched: {},
+    errors: {}
+  };
+
+  handleChange = event => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    event.persist();
+    this.setState(prevState => ({
+      values: {
+        ...prevState.values,
+        [name]: value,
+      },
+    }));
+  }
+
+  handleBlur = event => {
+    const target = event.target;
+    const name = target.name;
+    event.persist();
+    this.setState(prevState => ({
+      touched: {
+        ...prevState.touched,
+        [name]: true,
+      },
+    }));
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    // validate
+    this.props.onSubmit(this.state.values)
+  }
+
   render() {
-    return this.props.children({...this.state})
+    return this.props.children({
+      ...this.state,
+      handleChange: this.handleChange,
+      handleBlur: this.handleBlur,
+      handleSubmit: this.handleSubmit,
+    });
   }
 }
+
 
 class Reservation extends React.Component {
   constructor(props) {
@@ -16,45 +58,47 @@ class Reservation extends React.Component {
       isGoing: true,
       numberOfGuests: 2
     };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+    // this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   render() {
     return (
-      <MiniFormik>
-      {() => <form>
-        <label>
-          Is going:
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={this.state.isGoing}
-            onChange={this.handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Number of guests:
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange}
-          />
-        </label>
-      </form>
-      }
+      <MiniFormik
+        initialValues={{
+          isGoing: true,
+          numberOfGuests: 2,
+        }}
+        onSubmit={values => alert(JSON.stringify(values, null, 2))}
+        >
+        {(props) => {
+          const { values, errors, touched, handleBlur, handleSubmit, handleChange } = props
+          return (
+            <form onSubmit={handleSubmit}>
+              <label>
+                Is going:
+                <input
+                  name="isGoing"
+                  type="checkbox"
+                  className="checkbox"
+                  checked={values.isGoing}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+              </label>
+              <br />
+              <label>
+                Number of guests:
+                <input
+                  name="numberOfGuests"
+                  type="number"
+                  value={values.numberOfGuests}
+                  onChange={handleChange}
+                />
+              </label>
+              <pre>{JSON.stringify(props, null, 2)}</pre>
+          </form>
+        );
+      }}
       </MiniFormik>
     );
   }
